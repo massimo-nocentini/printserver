987f7b53d57659fd718b1b31051feb969f8ddc6b
Massimo Nocentini <massimo.nocentini@gmail.com>
2017-05-11 16:24:07 +0200

    Second version which uses asyncio.

--- client.py
+++ client.py
@@ -1,19 +1,26 @@
 
+import asyncio
 import socket
 import sys
 
 HOST, PORT = "localhost", 9100
 data = " ".join(sys.argv[1:])
 
-# Create a socket (SOCK_STREAM means a TCP socket)
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    # Connect to server and send data
-    sock.connect((HOST, PORT))
-    sock.sendall(bytes(data + "\n", "utf-8"))
-    sock.sendall(bytes(data + "\n", "utf-8"))
-    sock.sendall(bytes(data + "\n", "utf-8"))
-    # Receive data from the server and shut down
-    #received = str(sock.recv(1024), "utf-8")
+async def tcp_echo_client(message, loop):
 
-print("Client sent:     {}".format(data))
-#print("Client received: {}".format(received))
+    reader, writer = await asyncio.open_connection(HOST, PORT, loop=loop)
+
+    print('Send: %r' % message)
+    writer.write(message.encode())
+    await writer.drain()
+    writer.close()
+
+    data = await reader.read()
+    print('Received: %r' % data.decode())
+
+    print('Close the socket')
+    writer.close()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(tcp_echo_client(message=data, loop=loop))
+loop.close()
--- printserver.py
+++ printserver.py
@@ -1,45 +1,60 @@
 
+
+
 import socketserver, json, tempfile, subprocess
+from collections import defaultdict
+import asyncio
+from contextlib import suppress
 
 def make_handler(command_template, association):
 
     port, printer_name = association
 
-    class TCPPrintHandler(socketserver.BaseRequestHandler):
+    queue = defaultdict(list)
 
-        def handle(self):
-            data = []
-            while True:
-                chunk = self.request.recv(1024)
-                if not chunk: 
-                    break
-                data.append(chunk)
+    async def handler(reader, writer):
+        
+        while True:
 
-            data = b''.join(data)
+            data = await reader.read(1024)
+            client = writer.get_extra_info('peername')
 
-            print("{} wrote:\n{}".format(self.client_address, data.strip()))
-            self.do_print(data) # do the processing
+            if data:
+                queue[client].append(data)
+            else:
+                whole_message = b''.join(queue[client])
+                message = whole_message.decode()
+                print("Received {} from {}".format(message, client))
 
-            self.request.sendall(b'request accepted, dude!')
+                del queue[client]
+                do_print(whole_message)
+                
+                break
 
-        def do_print(self, data):
-            with tempfile.NamedTemporaryFile() as temp:
-                temp.write(data)
-                temp.flush()
-                command = command_template.format(filename=temp.name, printer_name=printer_name)
-                if port == 9100:
-                    subprocess.run(['cat', temp.name])
-                else:
-                    subprocess.run(command.split(' '))
-            
-            print('Print command executed: {}'.format(command))
+        print('out of loop')
+        try:
+            writer.write(b'Request processed, bye')
+            await writer.drain()
+        except (OSError, ConnectionError):
+            pass
 
-    return TCPPrintHandler # pay attention, the *class* object is required, not an instance of it
+    def do_print(data):
+        with tempfile.NamedTemporaryFile() as temp:
+            temp.write(data)
+            temp.flush()
+            command = command_template.format(filename=temp.name, printer_name=printer_name)
+            if port == 9100:
+                subprocess.run(['cat', temp.name])
+            else:
+                subprocess.run(command.split(' '))
+        
+        print('Print command executed: {}'.format(command))
+
+    return handler
 
 #________________________________________________________________________ 
 
-
-def start_server(HOST="127.0.0.1"):
+def make_queues(loop, HOST="127.0.0.1"):
 
     with open('printers_conf.json', 'r') as conf_file:
         conf = json.load(conf_file)
@@ -47,16 +62,26 @@ def start_server(HOST="127.0.0.1"):
         associations = {line['port']:line['printer'] for line in conf['queues']}
         print('Server configuration:\n{}'.format(json.dumps(conf, indent=4)))
 
-    for port, printer in associations.items():
-        with socketserver.TCPServer((HOST, port), make_handler(command_template, (port, printer))) as server:
-            server.serve_forever()
-
+    return asyncio.gather(*[asyncio.start_server(make_handler(command_template, (port, printer)), HOST, port, loop=loop) 
+                          for port, printer in associations.items()])
 
 if __name__ == "__main__":
-    try:
-        start_server()
-    except KeyboardInterrupt:
-        print('server shutdown')
+    loop = asyncio.get_event_loop()
+    coros_pool = make_queues(loop)
+    servers = loop.run_until_complete(coros_pool)
+
+    # Serve requests until Ctrl+C is pressed
+    print('Serving on sockets {}'.format(', '.join(str(s.sockets[0].getsockname()) for s in servers)))
+    with suppress(KeyboardInterrupt):
+        loop.run_forever()
+
+    # Close the server
+    for server in servers:
+        server.close()
+        loop.run_until_complete(server.wait_closed())
+
+    loop.close()
+    print("Server down.")
 
 
 
